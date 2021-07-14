3089711839a9c7ac4daad96652d38b530b2805b9
james.zz <james.zz@alibaba-inc.com>
2021-07-14 13:45:42 +0800

    [to #35377611] feat: bufferize DotOp and DotGeneralOp.
    Link: https://code.aone.alibaba-inc.com/algo/tensorflow_google/codereview/5885662

--- tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/lhlo/transforms/map_hlo_to_lhlo_op.h
+++ tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/lhlo/transforms/map_hlo_to_lhlo_op.h
@@ -58,6 +58,7 @@ MAP_HLO_TO_LHLO(CosineOp);
 MAP_HLO_TO_LHLO(CustomCallOp);
 MAP_HLO_TO_LHLO(DivOp);
 MAP_HLO_TO_LHLO(DotOp);
+MAP_HLO_TO_LHLO(DotGeneralOp);
 MAP_HLO_TO_LHLO(DynamicBroadcastInDimOp);
 MAP_HLO_TO_LHLO(DynamicGatherOp);
 MAP_HLO_TO_LHLO(DynamicIotaOp);
