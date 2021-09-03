1d073b555bd6e2eed4f605a9ad9516d201adb5c3
yancey.yx <yancey.yx@antfin.com>
2021-09-03 14:37:20 +0800

    [to #36574644] [MLIR] [DISC] Add reverse op in lmhlo
    Link: https://code.aone.alibaba-inc.com/algo/tensorflow_google/codereview/6364791
    
    * add reverse op in lmhlo

--- tensorflow/compiler/mlir/hlo/lib/Dialect/mhlo/IR/hlo_ops.cc
+++ tensorflow/compiler/mlir/hlo/lib/Dialect/mhlo/IR/hlo_ops.cc
@@ -4095,6 +4095,13 @@ OpFoldResult ReverseOp::fold(ArrayRef<Attribute> operands) {
   return input;
 }
 
+LogicalResult ReverseOp::reifyReturnTypeShapes(
+    OpBuilder& builder, ValueRange operands,
+    SmallVectorImpl<Value>& reifiedReturnShapes) {
+  return deriveShapeFromOperand(&builder, getOperation(), operands.front(),
+                                &reifiedReturnShapes);
+}
+
 //===----------------------------------------------------------------------===//
 // ReduceOp
 //===----------------------------------------------------------------------===//
