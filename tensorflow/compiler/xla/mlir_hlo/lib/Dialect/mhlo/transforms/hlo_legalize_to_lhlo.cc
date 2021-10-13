2acc7e4b1dccf83f159657573a0bac1c2b1b3b8a
kevin.zwy <kevin.zwy@alibaba-inc.com>
2021-10-13 16:10:04 +0800

    [to #37276187] feat: bufferize mhlo.log1pOp
    Link: https://code.aone.alibaba-inc.com/algo/tensorflow_google/codereview/6675686

--- tensorflow/compiler/xla/mlir_hlo/lib/Dialect/mhlo/transforms/hlo_legalize_to_lhlo.cc
+++ tensorflow/compiler/xla/mlir_hlo/lib/Dialect/mhlo/transforms/hlo_legalize_to_lhlo.cc
@@ -544,6 +544,7 @@ void populateHloToLhloConversionPattern(
       HloToLhloOpConverter<mhlo::IotaOp>,
       HloToLhloOpConverter<mhlo::IsFiniteOp>,
       HloToLhloOpConverter<mhlo::LogOp>,
+      HloToLhloOpConverter<mhlo::Log1pOp>,
       HloToLhloOpConverter<mhlo::LogisticOp>,
       HloToLhloOpConverter<mhlo::MaxOp>,
       HloToLhloOpConverter<mhlo::MinOp>,
