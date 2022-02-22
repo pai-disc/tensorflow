433158710d4fdbe94bac761eb9ff45462965255d
kevin.zwy <kevin.zwy@alibaba-inc.com>
2022-02-22 19:13:21 +0800

    fix

--- tensorflow/compiler/xla/service/gpu/gpu_executable.h
+++ tensorflow/compiler/xla/service/gpu/gpu_executable.h
@@ -136,6 +136,7 @@ class GpuExecutable : public Executable {
     // prevents the initial execution step from being slowed down due to
     // initializing GPU resources.
     std::shared_ptr<BufferAssignment> buffer_assignment_ =
+    std::shared_ptr<const BufferAssignment> buffer_assignment_ =
         nullptr; /*ADDED_FOR_TAO*/
   };
 
@@ -213,8 +214,8 @@ class GpuExecutable : public Executable {
 #if BEF_EXECUTABLE
     return nullptr;
 #else   //  BFD_EXECUTABLE
-    if (absl::holds_alternative<OwnedThunkSchedule>(thunks_or_bef_)) {
-      return absl::get<OwnedThunkSchedule>(thunks_or_bef_).get();
+    if (thunks_) {
+      return thunks_.get();
     }
     return nullptr;
 #endif  //  BFD_EXECUTABLE
@@ -328,7 +329,7 @@ class GpuExecutable : public Executable {
   GpuExecutable(const GpuExecutable&) = delete;
   GpuExecutable& operator=(const GpuExecutable&) = delete;
 
-  std::unique_ptr<BufferAssignment> buffer_assignment_; /*ADDED_FOR_TAO*/
+  std::shared_ptr<const BufferAssignment> buffer_assignment_; /*ADDED_FOR_TAO*/
 };
 
 StatusOr<absl::flat_hash_map<ShapeIndex, GpuExecutable::OutputInfo>>
